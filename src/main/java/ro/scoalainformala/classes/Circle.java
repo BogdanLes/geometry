a15f7b7c671b0128151435153dba586b90f176bd
BogdanLes <bogdan19802010@gmail.com>
2023-09-25 20:40:07 +0300

    Circle Class File

--- src/main/java/ro/scoalainformala/classes/Circle.java
+++ src/main/java/ro/scoalainformala/classes/Circle.java
@@ -1,2 +1,29 @@
-package ro.scoalainformala.classes;public class circle {
+package ro.scoalainformala.classes;
+
+public class Circle {
+
+    private float radius;
+    private float areaCircle;
+    private float diameterCircle;
+    private float circumferenceCircle;
+
+    public void setRadius(float rad) {
+        radius = rad;
+        areaCircle = 3.14F * radius * radius;
+        diameterCircle = 2 * radius;
+        circumferenceCircle = 2 * 3.14F * radius;
+    }
+
+    public float getAreaCircle() {
+        return areaCircle;
+    }
+
+    public float getDiameterCircle() {
+        return diameterCircle;
+    }
+
+    public float getCircumferenceCircle() {
+        return circumferenceCircle;
+    }
+
 }
